048922a451a926f08d1958fbae3f51826051fd54
Carolina Ferreira <ninaferreira2409@gmail.com>
2023-09-21 19:07:00 -0300

    add: colore intersecção

--- Trabalho_01/Voronoi/ExibeVARIOSPoligonos.cpp
+++ Trabalho_01/Voronoi/ExibeVARIOSPoligonos.cpp
@@ -45,7 +45,7 @@ using namespace std;
 #include "Temporizador.h"
 
 Temporizador T;
-double AccumDeltaT=0;
+double AccumDeltaT = 0;
 
 Poligono Pontos;
 
@@ -60,19 +60,19 @@ Ponto andante;
 bool desenha = false;
 bool FoiClicado = false;
 
-float angulo=0.0;
+float angulo = 0.0;
 
 // **********************************************************************
 //
 // **********************************************************************
 void printString(string s, int posX, int posY)
 {
-    //defineCor(cor);
-    glColor3f(1,1,1);
-    glRasterPos3i(posX, posY, 0); //define posicao na tela
+    // defineCor(cor);
+    glColor3f(1, 1, 1);
+    glRasterPos3i(posX, posY, 0); // define posicao na tela
     for (int i = 0; i < s.length(); i++)
     {
-//GLUT_BITMAP_HELVETICA_10, GLUT_BITMAP_TIMES_ROMAN_24,GLUT_BITMAP_HELVETICA_18
+        // GLUT_BITMAP_HELVETICA_10, GLUT_BITMAP_TIMES_ROMAN_24,GLUT_BITMAP_HELVETICA_18
         glutBitmapCharacter(GLUT_BITMAP_TIMES_ROMAN_24, s[i]);
     }
 }
@@ -81,13 +81,13 @@ void printString(string s, int posX, int posY)
 // **********************************************************************
 void ImprimeNumeracaoDosVertices(Poligono &P)
 {
-    for(int i=0;i<P.getNVertices();i++)
+    for (int i = 0; i < P.getNVertices(); i++)
     {
         Ponto aux;
         aux = P.getVertice(i);
         char msg[10];
-        sprintf(msg,"%d",i);
-        printString(msg,aux.x, aux.y);
+        sprintf(msg, "%d", i);
+        printString(msg, aux.x, aux.y);
     }
 }
 // **********************************************************************
@@ -97,15 +97,15 @@ void GeraPontos(int qtd)
 {
     time_t t;
     Ponto Escala;
-    Escala = (Max - Min) * (1.0/1000.0);
-    srand((unsigned) time(&t));
-    for (int i = 0;i<qtd; i++)
+    Escala = (Max - Min) * (1.0 / 1000.0);
+    srand((unsigned)time(&t));
+    for (int i = 0; i < qtd; i++)
     {
         float x = rand() % 1000;
         float y = rand() % 1000;
         x = x * Escala.x + Min.x;
         y = y * Escala.y + Min.y;
-        Pontos.insereVertice(Ponto(x,y));
+        Pontos.insereVertice(Ponto(x, y));
     }
 }
 
@@ -119,7 +119,7 @@ void init()
     glClearColor(0.0f, 0.0f, 1.0f, 1.0f);
 
     Voro.LePoligonos("ListaDePoligonos-V2.txt");
-    Voro.obtemLimites(Min,Max);
+    Voro.obtemLimites(Min, Max);
     Min.imprime("Minimo:", "\n");
     Max.imprime("Maximo:", "\n");
 
@@ -129,8 +129,8 @@ void init()
 
     CoresDosPoligonos = new int[Voro.getNPoligonos()];
 
-    for (int i=0; i<Voro.getNPoligonos(); i++)
-        CoresDosPoligonos[i] = i*2;//rand()%80;
+    for (int i = 0; i < Voro.getNPoligonos(); i++)
+        CoresDosPoligonos[i] = i * 2; // rand()%80;
 
     // Ajusta a largura da janela l�gica
     // em fun��o do tamanho dos pol�gonos
@@ -139,11 +139,10 @@ void init()
 
     Min = Min - Largura * 0.1;
     Max = Max + Largura * 0.1;
-
 }
 
-double nFrames=0;
-double TempoTotal=0;
+double nFrames = 0;
+double TempoTotal = 0;
 // **********************************************************************
 //
 // **********************************************************************
@@ -155,17 +154,17 @@ void animate()
     TempoTotal += dt;
     nFrames++;
 
-    if (AccumDeltaT > 1.0/30) // fixa a atualiza��o da tela em 30
+    if (AccumDeltaT > 1.0 / 30) // fixa a atualiza��o da tela em 30
     {
         AccumDeltaT = 0;
-        //angulo+=0.05;
+        // angulo+=0.05;
         glutPostRedisplay();
     }
     if (TempoTotal > 50.0)
     {
-        cout << "Tempo Acumulado: "  << TempoTotal << " segundos. " ;
+        cout << "Tempo Acumulado: " << TempoTotal << " segundos. ";
         cout << "Nros de Frames sem desenho: " << nFrames << endl;
-        cout << "FPS(sem desenho): " << nFrames/TempoTotal << endl;
+        cout << "FPS(sem desenho): " << nFrames / TempoTotal << endl;
         TempoTotal = 0;
         nFrames = 0;
     }
@@ -174,7 +173,7 @@ void animate()
 //  void reshape( int w, int h )
 //  trata o redimensionamento da janela OpenGL
 // **********************************************************************
-void reshape( int w, int h )
+void reshape(int w, int h)
 {
     // Reset the coordinate system before modifying
     glMatrixMode(GL_PROJECTION);
@@ -182,9 +181,9 @@ void reshape( int w, int h )
     // Define a area a ser ocupada pela area OpenGL dentro da Janela
     glViewport(0, 0, w, h);
     // Define os limites logicos da area OpenGL dentro da Janela
-    glOrtho(Min.x,Max.x,
-            Min.y,Max.y,
-            0,1);
+    glOrtho(Min.x, Max.x,
+            Min.y, Max.y,
+            0, 1);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
@@ -195,17 +194,17 @@ void reshape( int w, int h )
 void DesenhaEixos()
 {
     Ponto Meio;
-    Meio.x = (Max.x+Min.x)/2;
-    Meio.y = (Max.y+Min.y)/2;
-    Meio.z = (Max.z+Min.z)/2;
+    Meio.x = (Max.x + Min.x) / 2;
+    Meio.y = (Max.y + Min.y) / 2;
+    Meio.z = (Max.z + Min.z) / 2;
 
     glBegin(GL_LINES);
     //  eixo horizontal
-        glVertex2f(Min.x,Meio.y);
-        glVertex2f(Max.x,Meio.y);
+    glVertex2f(Min.x, Meio.y);
+    glVertex2f(Max.x, Meio.y);
     //  eixo vertical
-        glVertex2f(Meio.x,Min.y);
-        glVertex2f(Meio.x,Max.y);
+    glVertex2f(Meio.x, Min.y);
+    glVertex2f(Meio.x, Max.y);
     glEnd();
 }
 // **********************************************************************
@@ -214,8 +213,8 @@ void DesenhaEixos()
 void DesenhaLinha(Ponto P1, Ponto P2)
 {
     glBegin(GL_LINES);
-        glVertex3f(P1.x,P1.y,P1.z);
-        glVertex3f(P2.x,P2.y,P2.z);
+    glVertex3f(P1.x, P1.y, P1.z);
+    glVertex3f(P2.x, P2.y, P2.z);
     glEnd();
 }
 // **********************************************************************
@@ -223,7 +222,7 @@ void DesenhaPonto(Ponto P, int tamanho)
 {
     glPointSize(tamanho);
     glBegin(GL_POINTS);
-        glVertex3f(P.x,P.y,P.z);
+    glVertex3f(P.x, P.y, P.z);
     glEnd();
 }
 // **********************************************************************
@@ -238,40 +237,39 @@ void InterseptaArestas(Poligono P)
         if (HaInterseccao(PontoClicado,Esq, P1, P2))
             P.desenhaAresta(i);
     }*/
-
 }
 // **********************************************************************
 //  void display( void )
 // **********************************************************************
-void display( void )
+void display(void)
 {
-	// Limpa a tela coma cor de fundo
-	glClear(GL_COLOR_BUFFER_BIT);
+    // Limpa a tela coma cor de fundo
+    glClear(GL_COLOR_BUFFER_BIT);
 
     // Define os limites l�gicos da area OpenGL dentro da Janela
-	glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 
-	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-	// Coloque aqui as chamadas das rotinas que desenham os objetos
-	// <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+    // Coloque aqui as chamadas das rotinas que desenham os objetos
+    // <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
-	glLineWidth(1);
-	glColor3f(1,1,1); // R, G, B  [0..1]
+    glLineWidth(1);
+    glColor3f(1, 1, 1); // R, G, B  [0..1]
     DesenhaEixos();
 
-    glRotatef(angulo, 0,0,1);
+    glRotatef(angulo, 0, 0, 1);
     glLineWidth(2);
 
     Poligono P;
-    for (int i=0; i<Voro.getNPoligonos(); i++)
+    for (int i = 0; i < Voro.getNPoligonos(); i++)
     {
         defineCor(CoresDosPoligonos[i]);
         P = Voro.getPoligono(i);
         P.pintaPoligono();
     }
-    glColor3f(0,0,0);
-    for (int i=0; i<Voro.getNPoligonos(); i++)
+    glColor3f(0, 0, 0);
+    for (int i = 0; i < Voro.getNPoligonos(); i++)
     {
         P = Voro.getPoligono(i);
         P.desenhaPoligono();
@@ -281,24 +279,36 @@ void display( void )
     {
         desenha = false;
     }
-    if (FoiClicado == true)
-    {
-        Ponto Esq;
-        Ponto Dir (-1,0);
-        Esq = PontoClicado + Dir * (1000);
-        glColor3f(0,1,0); // R, G, B  [0..1]
-        DesenhaLinha(PontoClicado, Esq);
-        DesenhaPonto(PontoClicado, 3);
 
-        glColor3f(1,0,0); // R, G, B  [0..1]
+    Ponto Esq;
+    Ponto Dir(-1, 0);
+    Esq = andante + Dir * (1000);
+    glColor3f(0, 1, 0); // R, G, B  [0..1]
+    DesenhaLinha(andante, Esq);
+    glColor3f(0, 1, 0);
+    DesenhaPonto(andante, 3);
 
-
-    }
+    glColor3f(1, 0, 0); // R, G, B  [0..1]
 
     DesenhaPonto(andante, 10);
-    //Mapa.desenhaVertices();
-    //glColor3f(1,0,0); // R, G, B  [0..1]
-    //DesenhaLinha(Mapa.getVertice(0), Ponto(Min.x, Max.y));
+
+    glColor3f(0, 1, 0);
+    Ponto P1, P2;
+    for (int i = 0; i < Voro.getNPoligonos(); i++)
+    {
+        Poligono Map = Voro.getPoligono(i);
+        for (int j = 0; j < Map.getNVertices(); j++)
+        {
+            Map.getAresta(j, P1, P2);
+            if (HaInterseccao(andante, Esq, P1, P2))
+            {
+                Map.desenhaAresta(j);
+            }
+        }
+    }
+    // Mapa.desenhaVertices();
+    // glColor3f(1,0,0); // R, G, B  [0..1]
+    // DesenhaLinha(Mapa.getVertice(0), Ponto(Min.x, Max.y));
 
     glutSwapBuffers();
 }
@@ -313,7 +323,7 @@ void ContaTempo(double tempo)
 
     unsigned long cont = 0;
     cout << "Inicio contagem de " << tempo << "segundos ..." << flush;
-    while(true)
+    while (true)
     {
         tempo -= T.getDeltaT();
         cont++;
@@ -325,20 +335,22 @@ void ContaTempo(double tempo)
     }
 }
 
-bool pontoNoPoligono(Ponto &pt, Poligono &p) {
+bool pontoNoPoligono(Ponto &pt, Poligono &p)
+{
     Ponto pontoMenor, pontoMaior;
     p.obtemLimites(pontoMenor, pontoMaior);
     Envelope envelope = Envelope(pontoMenor, pontoMaior);
     return envelope.pontoEstaDentro(pt);
-  //  if (pt.x > pontoMenor.x && pt.x < pontoMaior.x) {
-  //      if (pt.y > pontoMenor.y && pt.y < pontoMaior.y) {
-  //          return true;
-  //      }
-  //  }
-   // return false;
+    //  if (pt.x > pontoMenor.x && pt.x < pontoMaior.x) {
+    //      if (pt.y > pontoMenor.y && pt.y < pontoMaior.y) {
+    //          return true;
+    //      }
+    //  }
+    // return false;
 }
 
-void checkPointPosition(Ponto &p, float x, float y) {
+void checkPointPosition(Ponto &p, float x, float y)
+{
     if (p.x > Max.x)
         p.x = Min.x;
     else if (p.x < Min.x)
@@ -346,108 +358,114 @@ void checkPointPosition(Ponto &p, float x, float y) {
 
     if (p.y > Max.y)
         p.y = Min.y;
-    else if (p.y < Min.y) {
-          p.y = Max.y;
+    else if (p.y < Min.y)
+    {
+        p.y = Max.y;
     }
-        glutPostRedisplay();
-        Ponto P1, P2;
-        Poligono P;
-        Ponto Esq;
-        Ponto Dir (-1,0);
-        Esq = PontoClicado + Dir * (1000);
-        for (int i=0; i<Voro.getNPoligonos(); i++)
+    glutPostRedisplay();
+    Ponto P1, P2;
+    Poligono P;
+    Ponto Esq;
+    Ponto Dir(-1, 0);
+    Esq = p + Dir * (1000);
+    DesenhaLinha(Esq, p);
+    for (int i = 0; i < Voro.getNPoligonos(); i++)
+    {
+        P = Voro.getPoligono(i);
+        for (int j = 0; j < P.getNVertices(); j++)
         {
-            P = Voro.getPoligono(i);
-            for (int j=0; j<P.getNVertices();j++)
+            P.getAresta(j, P1, P2);
+
+            if (HaInterseccao(p, Esq, P1, P2))
             {
-                P.getAresta(j, P1, P2);
-
-                if (HaInterseccao(PontoClicado,Esq, P1, P2)) {
-                     cout << "\nAchei: " << j;
-                     P.desenhaAresta(j);
-                }
-
+                cout << "\nAchei: " << j;
+                P.desenhaAresta(j);
             }
         }
-        glutPostRedisplay();
+    }
+    glutPostRedisplay();
 }
 
-void movePointVertical(Ponto &p, float distance) {
+void movePointVertical(Ponto &p, float distance)
+{
     p.y += distance;
     checkPointPosition(p, 0, distance);
 }
 
-void movePointHorizontal(Ponto &p, float distance) {
+void movePointHorizontal(Ponto &p, float distance)
+{
     p.x += distance;
     checkPointPosition(p, distance, 0);
 }
 
-void movePoint(char key) {
-    switch (key) {
-        case 'w':
-            cout << "Clicou w";
-            movePointVertical(andante, 0.1);
-            break;
-        case 'a':
-            cout << "Clicou a";
-            movePointHorizontal(andante, -0.1);
-            break;
-        case 's':
-            cout << "Clicou s";
-            movePointVertical(andante, -0.1);
-            break;
-        case 'd':
-            cout << "Clicou w";
-            movePointHorizontal(andante, 0.1);
-            break;
+void movePoint(char key)
+{
+    switch (key)
+    {
+    case 'w':
+        cout << "Clicou w";
+        movePointVertical(andante, 0.1);
+        break;
+    case 'a':
+        cout << "Clicou a";
+        movePointHorizontal(andante, -0.1);
+        break;
+    case 's':
+        cout << "Clicou s";
+        movePointVertical(andante, -0.1);
+        break;
+    case 'd':
+        cout << "Clicou w";
+        movePointHorizontal(andante, 0.1);
+        break;
     }
 }
 // **********************************************************************
 //  void keyboard ( unsigned char key, int x, int y )
 // **********************************************************************
-void keyboard ( unsigned char key, int x, int y )
+void keyboard(unsigned char key, int x, int y)
 {
-	switch ( key )
-	{
-		case 27:        // Termina o programa qdo
-			exit ( 0 );   // a tecla ESC for pressionada
-			break;
-        case 'w':
-        case 'a':
-        case 's':
-        case 'd':
-            movePoint(key);
-            break;
-        case 't':
-            ContaTempo(3);
-            break;
-        case ' ':
-            desenha = !desenha;
-            break;
-		default:
-			break;
-	}
+    switch (key)
+    {
+    case 27:     // Termina o programa qdo
+        exit(0); // a tecla ESC for pressionada
+        break;
+    case 'w':
+    case 'a':
+    case 's':
+    case 'd':
+        movePoint(key);
+        break;
+    case 't':
+        ContaTempo(3);
+        break;
+    case ' ':
+        desenha = !desenha;
+        break;
+    default:
+        break;
+    }
 
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 // **********************************************************************
 //  void arrow_keys ( int a_keys, int x, int y )
 // **********************************************************************
-void arrow_keys ( int a_keys, int x, int y )
+void arrow_keys(int a_keys, int x, int y)
 {
-	switch ( a_keys )
-	{
-		case GLUT_KEY_UP:       // Se pressionar UP
-			glutFullScreen ( ); // Vai para Full Screen
-			break;
-	    case GLUT_KEY_DOWN:     // Se pressionar UP
-								// Reposiciona a janela
-            glutPositionWindow (50,50);
-			glutReshapeWindow ( 700, 500 );
-			break;
-		default:
-			break;
-	}
+    switch (a_keys)
+    {
+    case GLUT_KEY_UP:     // Se pressionar UP
+        glutFullScreen(); // Vai para Full Screen
+        break;
+    case GLUT_KEY_DOWN: // Se pressionar UP
+                        // Reposiciona a janela
+        glutPositionWindow(50, 50);
+        glutReshapeWindow(700, 500);
+        break;
+    default:
+        break;
+    }
 }
 // **********************************************************************
 // Esta fun��o captura o clique do botao direito do mouse sobre a �rea de
@@ -455,59 +473,58 @@ void arrow_keys ( int a_keys, int x, int y )
 // na glOrtho (ver fun��o reshape)
 // Este c�digo � baseado em http://hamala.se/forums/viewtopic.php?t=20
 // **********************************************************************
-void Mouse(int button,int state,int x,int y)
+void Mouse(int button, int state, int x, int y)
 {
     GLint viewport[4];
-    GLdouble modelview[16],projection[16];
-    GLfloat wx=x,wy,wz;
-    GLdouble ox=0.0,oy=0.0,oz=0.0;
+    GLdouble modelview[16], projection[16];
+    GLfloat wx = x, wy, wz;
+    GLdouble ox = 0.0, oy = 0.0, oz = 0.0;
 
-    if(state!=GLUT_DOWN)
-      return;
-    if(button!=GLUT_LEFT_BUTTON)
-     return;
+    if (state != GLUT_DOWN)
+        return;
+    if (button != GLUT_LEFT_BUTTON)
+        return;
     cout << "Botao da Esquerda! ";
 
-    glGetIntegerv(GL_VIEWPORT,viewport);
-    y=viewport[3]-y;
-    wy=y;
-    glGetDoublev(GL_MODELVIEW_MATRIX,modelview);
-    glGetDoublev(GL_PROJECTION_MATRIX,projection);
-    glReadPixels(x,y,1,1,GL_DEPTH_COMPONENT,GL_FLOAT,&wz);
-    gluUnProject(wx,wy,wz,modelview,projection,viewport,&ox,&oy,&oz);
-    PontoClicado = Ponto(ox,oy,oz);
+    glGetIntegerv(GL_VIEWPORT, viewport);
+    y = viewport[3] - y;
+    wy = y;
+    glGetDoublev(GL_MODELVIEW_MATRIX, modelview);
+    glGetDoublev(GL_PROJECTION_MATRIX, projection);
+    glReadPixels(x, y, 1, 1, GL_DEPTH_COMPONENT, GL_FLOAT, &wz);
+    gluUnProject(wx, wy, wz, modelview, projection, viewport, &ox, &oy, &oz);
+    PontoClicado = Ponto(ox, oy, oz);
     PontoClicado.imprime("- Ponto no universo: ", "\n");
     FoiClicado = true;
 }
-
 
 // **********************************************************************
 //  void main ( int argc, char** argv )
 //
 // **********************************************************************
-int  main ( int argc, char** argv )
+int main(int argc, char **argv)
 {
     cout << "Programa OpenGL" << endl;
 
-    glutInit            ( &argc, argv );
-    glutInitDisplayMode (GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB );
-    glutInitWindowPosition (0,0);
+    glutInit(&argc, argv);
+    glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGB);
+    glutInitWindowPosition(0, 0);
 
     // Define o tamanho inicial da janela grafica do programa
-    glutInitWindowSize  ( 650, 500);
+    glutInitWindowSize(650, 500);
 
     // Cria a janela na tela, definindo o nome da
     // que aparecera na barra de titulo da janela.
-    glutCreateWindow    ( "Poligonos em OpenGL" );
+    glutCreateWindow("Poligonos em OpenGL");
 
     // executa algumas inicializa��es
-    init ();
+    init();
 
     // Define que o tratador de evento para
     // o redesenho da tela. A funcao "display"
     // ser� chamada automaticamente quando
     // for necess�rio redesenhar a janela
-    glutDisplayFunc ( display );
+    glutDisplayFunc(display);
 
     // Define que o tratador de evento para
     // o invalida��o da tela. A funcao "display"
@@ -519,13 +536,13 @@ int  main ( int argc, char** argv )
     // o redimensionamento da janela. A funcao "reshape"
     // ser� chamada automaticamente quando
     // o usu�rio alterar o tamanho da janela
-    glutReshapeFunc ( reshape );
+    glutReshapeFunc(reshape);
 
     // Define que o tratador de evento para
     // as teclas. A funcao "keyboard"
     // ser� chamada automaticamente sempre
     // o usu�rio pressionar uma tecla comum
-    glutKeyboardFunc ( keyboard );
+    glutKeyboardFunc(keyboard);
 
     // Define que o tratador de evento para
     // as teclas especiais(F1, F2,... ALT-A,
@@ -533,12 +550,12 @@ int  main ( int argc, char** argv )
     // A funcao "arrow_keys" ser� chamada
     // automaticamente sempre o usu�rio
     // pressionar uma tecla especial
-    glutSpecialFunc ( arrow_keys );
+    glutSpecialFunc(arrow_keys);
 
     glutMouseFunc(Mouse);
 
     // inicia o tratamento dos eventos
-    glutMainLoop ( );
+    glutMainLoop();
 
     return 0;
 }
